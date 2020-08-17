4a7e3896af5971f981b5a8737301eb85fe6cd11c
Eyeruz <3yerus@gmail.com>
2020-08-17 08:05:48 -0400

    controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,52 @@
 class CategoriesController < ApplicationController
+    def index
+        # @items = Item.all
+        end
+        
+        
+        def new
+        # @item = Item.new
+        
+        end
+        
+        
+        def create
+        
+        # @item = Item.create(item_params)
+        # if @item.save
+        #     redirect_to item_path(@item)
+        end
+        
+        
+        def edit
+        
+        
+        end
+        
+        
+        def update
+        
+        
+        end
+        
+        
+        
+        def show
+        
+        
+        end
+        
+        
+        def delete
+        
+        
+        end
+        
+        private
+        
+        def item_params
+        
+            params.require(:item).permit( :size, :quanity, :color, :price)
+        end
+        
 end
